5029a982c921d31a7f808e14f98cd2568c64c8e8
iDelTer <ivandelter@outlook.com>
2023-07-26 20:04:14 +0200

    Start add movie

--- src/pages/Admin/Insertar/AdminContainers/ContainerAleatorio.jsx
+++ src/pages/Admin/Insertar/AdminContainers/ContainerAleatorio.jsx
@@ -1,20 +1,39 @@
 import { useState, useEffect } from "react";
-import { getRemoteMovies } from "../../../../services/movies";
-import './container_aleatorio.css'
+import { getRemoteMovies, addLocalMovie } from "../../../../services/movies";
+import { getUserLocalSession } from "../../../../utils/UserProfile";
+import "./container_aleatorio.css";
 
 function ContainerAleatorio() {
-	const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-	useEffect(() => {
-		const fetchRemoteMovies = async () => {
-			const films = await getRemoteMovies();
-			setMovies(films);
-		};
-		fetchRemoteMovies();
-	}, []);
+  useEffect(() => {
+    const fetchRemoteMovies = async () => {
+      const films = await getRemoteMovies();
+      setMovies(films);
+    };
+    fetchRemoteMovies();
+  }, []);
 
-	return (
-		<div id="peliculitas">
+  const addMovie = async (identifier) => {
+    // let sessionId = getUserLocalSession();
+    let bodyObj = {};
+    let movieItem = movies.find((item) => item.id === identifier);
+    console.log(movieItem);
+    bodyObj = {
+      name: movieItem.original_title,
+      description: movieItem.overview,
+      release: movieItem.release_date,
+    };
+    console.log(bodyObj);
+    bodyObj = JSON.stringify(bodyObj);
+    console.log(bodyObj);
+
+    const data = await addLocalMovie(bodyObj);
+    console.log(JSON.stringify(films));
+  };
+
+  return (
+    <div id="peliculitas">
       <table>
         <thead>
           <tr>
@@ -25,20 +44,33 @@ function ContainerAleatorio() {
           </tr>
         </thead>
         <tbody>
-			{movies.length > 0 && movies.map((item) => {
-				return (
-          <tr key={item.id}>
-            <td><img src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`} className="movie-film-image"/></td>
-            <td>{item.title}</td>
-            <td>{item.overview.substring(0, 120)}</td>
-            <td>botón</td>
-          </tr>
-        )
-			})}
-      </tbody>
+          {movies.length > 0 &&
+            movies.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>
+                    <img
+                      src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
+                      className="movie-film-image"
+                    />
+                  </td>
+                  <td>{item.title}</td>
+                  <td>{item.overview.substring(0, 120)}</td>
+                  <td>
+                    <i
+                      onClick={() => {
+                        addMovie(item.id);
+                      }}
+                      className="bi bi-cloud-plus"
+                    ></i>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
       </table>
-		</div>
-	);
+    </div>
+  );
 }
 
 export default ContainerAleatorio;
--- src/services/movies.js
+++ src/services/movies.js
@@ -1,23 +1,45 @@
-import { apiEndpointGetRemoteMovies } from "../utils/endpoints"
+import {
+  apiEndpointGetRemoteMovies,
+  apiEndpointAddMovie,
+} from "../utils/endpoints";
 import { getLocalUserSession } from "../utils/UserProfile";
 
 export const getRemoteMovies = async () => {
-    const userInfo = getLocalUserSession();
-    try{
-        const response = await fetch(apiEndpointGetRemoteMovies, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${userInfo.access_token}`,
-            }
-        });
+  const userInfo = getLocalUserSession();
+  try {
+    const response = await fetch(apiEndpointGetRemoteMovies, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.access_token}`,
+      },
+    });
 
-        const data = await response.json();
-        console.log(data)
+    const data = await response.json();
+    console.log(data);
 
-        return data;
-    }
-    catch (error){
-        console.error(error)
-    }
-}
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const addLocalMovie = async (bodyData) => {
+  const session = getLocalUserSession();
+  try {
+    const response = await fetch(apiEndpointAddMovie, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session}`,
+      },
+      body: bodyData,
+    });
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+    return error.message;
+  }
+};
